f5c2bba092beca3de22bac7be8bee18ee05cad72
joaopaulosf <jpforcode@gmail.com>
2023-05-25 10:48:04 -0300

    :lipstick: Add home-page for users not auth

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,20 +2,12 @@ import "./styles.css";
 import { getDocs, collection, orderBy, query } from "firebase/firestore";
 import { db } from "../../config/firebase";
 import { useEffect, useState } from "react";
-import { Post } from "./post";
 import arrow from "../../images/arrow-up.svg";
-
-export interface Post {
-  id: string;
-  userId: string;
-  title: string;
-  username: string;
-  description: string;
-  timestamp: any;
-}
+import { Post as PostI } from "../../config/interfaces";
+import { Post } from "./post";
 
 export const Home = () => {
-  const [postsList, setPostsList] = useState<Post[] | null>(null);
+  const [postsList, setPostsList] = useState<PostI[] | null>(null);
   const [backToTopBtn, setBackToTopBtn] = useState(false);
   // const postsRef = collection(db, "posts");
 
@@ -24,7 +16,7 @@ export const Home = () => {
       query(collection(db, "posts"), orderBy("timestamp", "desc"))
     );
     setPostsList(
-      data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[]
+      data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as PostI[]
     );
   };
 
